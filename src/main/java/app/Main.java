c4ca41ffba1c698f9aa6b261a1a097669ed9237e
reno-ir <renecas1996@gmail.com>
2025-11-26 15:43:34 -0500

    Se realiza el metodo de binario a decimal

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,9 +1,7 @@
 package app;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-
+    public static void main(String[] args) {
+        System.out.println(app.service.binarytodecimal.binaryToDecimal("10110")); // ejemplo: 11
     }
 }
